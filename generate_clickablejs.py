aff01bcf0b9f746229624006813ae5a61d2ed5e5
Amalie <amaliestokholm@gmail.com>
2025-01-30 22:52:10 +0000

    Snapshot

--- generate_clickablejs.py
+++ generate_clickablejs.py
@@ -58,7 +58,7 @@ html_content = """<!DOCTYPE html>
                             font: { family: "Verdana, sans-serif", size: 16, color: "darkred" }
                         },
                         type: "log",
-                        range: [0.1, 3]
+                        range: [-2, 3]
                     },
                     dragmode: false  // Disable zoom & panning
                 };
